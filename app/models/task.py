c6a208228cd8f0ebf92579008a9b38361bb726a1
Esther Kang <estherkang.j@gmail.com>
2025-05-09 01:41:04 -0700

    adds migration, implements nested routes

--- app/models/task.py
+++ app/models/task.py
@@ -1,6 +1,10 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
 from datetime import datetime
 from ..db import db
+from typing import Optional, TYPE_CHECKING
+if TYPE_CHECKING:
+    from .goal import Goal
 
 
 class Task(db.Model):
@@ -8,14 +12,19 @@ class Task(db.Model):
     title: Mapped[str]
     description: Mapped[str]
     completed_at: Mapped[datetime] = mapped_column(nullable=True)
+    goal_id: Mapped[Optional[int]] = mapped_column(ForeignKey("goal.id"))
+    goal: Mapped[Optional["Goal"]] = relationship(back_populates="tasks")
 
     def to_dict(self):
         task_as_dict = {
             "id": self.id,
             "title": self.title,
             "description": self.description,
-            "is_complete": True if self.completed_at else False
+            "is_complete": True if self.completed_at else False,
         }
+
+        if self.goal_id:
+            task_as_dict["goal_id"] = self.goal_id
 
         return task_as_dict
 
--- app/routes/goal_routes.py
+++ app/routes/goal_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, Response
 from app.models.goal import Goal
+from app.models.task import Task
 from .route_utilities import validate_model, create_model
 from .route_utilities import get_models_with_filters
 from ..db import db
@@ -44,3 +45,35 @@ def delete_task(id):
     db.session.commit()
 
     return Response(status=204, mimetype="application/json")
+
+
+@bp.post("/<goal_id>/tasks")
+def add_tasks_to_goal(goal_id):
+    goal = validate_model(Goal, goal_id)
+    request_body = request.get_json()
+
+    for task in goal.tasks:
+        task.goal_id = None
+
+    tasks = [validate_model(Task, id) for id in request_body["task_ids"]]
+
+    for task in tasks:
+        task.goal_id = goal_id
+
+    db.session.commit()
+
+    response = {
+        "id": goal.id,
+        "task_ids": request_body["task_ids"]
+    }
+
+    return response, 200
+
+
+@bp.get("/<goal_id>/tasks")
+def get_all_tasks_of_goal(goal_id):
+    goal = validate_model(Goal, goal_id)
+    response = goal.to_dict()
+    response["tasks"] = [task.to_dict() for task in goal.tasks]
+
+    return response, 200
